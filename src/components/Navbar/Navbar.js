28839dfceb908411f9ec3aba6f91a5ef0e2fa693
Raphael Lasplace <raphael.lasplace@yahoo.fr>
2024-04-09 12:06:04 +0200

    Implémentation du responsive de la navbar et de la logique du menu hamburger

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,21 @@
-import styles from "./navbar.css"
+"use client"
 import Image from "next/image"
 import Link from "next/link"
+import "./navbar.css"
+import React, { useState } from "react"
 
 export default function Navbar() {
+
+    const [active, setActive] = useState(false)
+
+    const handleToggle = () => {
+        setActive(!active)
+    }
+    console.log(active)
+
     return(
         <nav className="navbar">
-            <Link href="/">
+            <Link href="/" className="logo-link">
                 <Image 
                 src="/assets/shared/desktop/logo-dark.png" 
                 alt="Designo logo" 
@@ -15,7 +25,7 @@ export default function Navbar() {
             </Link>
 
             <div>
-                <ul  className="navbar-items">
+                <ul className={active ? "navbar-items active" : "navbar-items"}>
                     <Link href="/company">
                         <li>OUR COMPANY</li>
                     </Link>
@@ -26,6 +36,15 @@ export default function Navbar() {
                         <li>CONTACT</li>
                     </Link>
                 </ul>
+            </div>
+            <div className={active ? "opacity-overlay-active" : "opacity-overlay"}></div>
+
+            <div className="menu-hamburger" onClick={handleToggle}>
+                {!active ?
+                   <svg width="24" height="20" xmlns="http://www.w3.org/2000/svg"><g fill="#1D1C1E" fillRule="evenodd"><path d="M0 0h24v4H0zM0 8h24v4H0zM0 16h24v4H0z"/></g></svg>
+                :
+                    <svg width="20" height="20" xmlns="http://www.w3.org/2000/svg"><path d="M17.071.1L19.9 2.93l-7.071 7.07 7.071 7.072-2.828 2.828L10 12.828l-7.071 7.071L.1 17.071 7.17 10 .102 2.929 2.929.1l7.07 7.07 7.072-7.07z" fill="#1D1C1E" fillRule="evenodd"/></svg>
+                }
             </div>
             
         </nav>
