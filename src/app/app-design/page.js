5f7ab522781ef36d31e5af06d85a22574493f28b
Raphael Lasplace <raphael.lasplace@yahoo.fr>
2024-04-11 19:18:39 +0200

    Implémentation de la page App design

---
+++ src/app/app-design/page.js
@@ -0,0 +1,44 @@
+"use client"
+import data from "../data.json"
+import "../web-design/web-design.css"
+import Navbar from "@/components/Navbar/Navbar"
+import Card from "@/components/Card/Card"
+import Image from "next/image"
+import Categories from "@/components/Categories/categories"
+import Footer from "@/components/Footer/Footer"
+import Banner from "@/components/Banner/Banner"
+
+export default function webDesign() {
+
+    return(
+        <div className="pagesBase">
+            <Navbar/>
+
+            <Image 
+             src="/assets/shared/desktop/bg-pattern-leaf.svg"
+             width={1000}
+             height={590}
+             alt="Backrgound image"
+             className="pageBackground"
+            />
+
+            <Banner props={data.app.banner}/>
+
+            <div className="cardContainer">
+            {data.app.projects.map((project, index) => (
+                <Card key={index} props={project}/>
+            ))}
+            </div>
+
+            <div className="categories">
+                {data.app.others.map((category, index) => (
+                    <Categories key={index} props={category} />
+                ))}
+            </div>
+
+            <Footer />
+
+        </div>
+
+    )
+}
