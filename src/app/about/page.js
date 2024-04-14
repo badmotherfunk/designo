17b153600333c120b5909f85458186d3616f53bb
Raphael Lasplace <raphael.lasplace@yahoo.fr>
2024-04-14 10:31:24 +0200

    Implémentation du background de la page about et du responsive

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -50,8 +50,15 @@ export default function Company() {
                         <p>Our team is multi-disciplinary and we are not merely interested in form — content and meaning are just as important. We give great importance to craftsmanship, service, and prompt delivery. Clients have always been impressed with our high-quality outcomes that encapsulates their brand’s story and mission.</p>
                     </div>
                 </div>
-
             </div>
+
+            <Image 
+             src="/assets/shared/desktop/bg-pattern-leaf.svg"
+             width={1000}
+             height={590}
+             alt="Backrgound image"
+             className="backgroundImageRight"
+            />
 
         </div>
     )
