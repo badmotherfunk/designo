00c6de51737008fcb1bb5149b9c043764df85ca1
Raphael Lasplace <raphael.lasplace@yahoo.fr>
2024-04-15 10:15:34 +0200

    Implémentation de la dernieère section de la page about et gestion du responsive

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,7 +1,8 @@
 import Navbar from "@/components/Navbar/Navbar"
 import "./about.css"
 import Image from "next/image"
-import Cities from "@/components/Cities/cities"
+import Cities from "@/components/Cities/Cities"
+import Footer from "@/components/Footer/Footer"
 
 export default function Company() {
     return(
@@ -62,6 +63,29 @@ export default function Company() {
             />
 
             <Cities />
+
+            <div className="aboutInfo">
+
+                <div className="aboutInfoContainer">
+                    <div className="aboutInfoContent">
+                        <h1 className="aboutInfoTitle">The real deal</h1>
+                        <p className="aboutInfoText">As strategic partners in our clients’ businesses, we are ready to take on any challenge as our own. Solving real problems require empathy and collaboration, and we strive to bring a fresh perspective to every opportunity. We make design and technology more accessible and give you tools to measure success.</p>
+                        <p>We are visual storytellers in appealing and captivating ways. By combining business and marketing strategies, we inspire audiences to take action and drive real results.</p>
+                    </div>
+                </div>
+
+                <Image 
+                 src="/assets/about/desktop/image-real-deal.jpg"
+                 width={0}
+                 height={0}
+                 sizes="100%"
+                 alt="Woman seen from behind contemplating frames"
+                 className="aboutInfoImage"
+                />
+            </div>
+
+
+            <Footer />
 
         </div>
     )
