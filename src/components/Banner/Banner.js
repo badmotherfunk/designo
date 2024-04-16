a576099265a228da1008754255768e8076f054fd
Raphael Lasplace <raphael.lasplace@yahoo.fr>
2024-04-16 08:32:45 +0200

    Modification de la class du hero pour les différentes pages

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,13 +1,16 @@
 "use client"
 
 import "./banner.css"
+import { usePathname } from "next/navigation"
 
 export default function Banner({props}) {
+
+    const pathname = usePathname()
     
     const {title, description} = props[0]
 
     return(
-        <div className="heroContainer">
+        <div className={pathname === "/web-design" ? "heroContainer" : "heroContainer left"}>
             <h1 className="heroTitle">{title}</h1>
             <p className="heroText">{description}</p>
         </div>     
