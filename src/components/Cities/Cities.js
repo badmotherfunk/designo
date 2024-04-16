a922d61af2551a1edd9070a6dae9a2a26bf31c4c
Raphael Lasplace <raphael.lasplace@yahoo.fr>
2024-04-16 08:57:23 +0200

    Modification du positionnement sur l'image Canada

--- src/components/Cities/Cities.js
+++ src/components/Cities/Cities.js
@@ -13,9 +13,9 @@ export default function Cities() {
               src="/assets/shared/desktop/illustration-canada.svg"
               width={202}
               height={202}
-              alt="Man and women working together"
+              alt="CN Tower canada illustration"
             />
-            <svg className="citiesBackgroundLeft" width="215" height="220" viewBox="0 0 650 550" xmlns="http://www.w3.org/2000/svg"><defs><linearGradient x1="0%" y1="50%" x2="100%"y2="50%" id="a"><stop stopColor="#5D0202" stopOpacity="0"offset="0%"/><stop stopColor="#5D0202" stopOpacity=".498"offset="100%"/></linearGradient></defs><circle fill="url(#a)"transform="matrix(0 -1 -1 0 640 640)" cx="320" cy="320" r="320"fillRule="evenodd" opacity=".309"/></svg>
+            <svg className="citiesBackgroundLeft" width="220" height="220" viewBox="0 0 650 690" xmlns="http://www.w3.org/2000/svg"><defs><linearGradient x1="0%" y1="50%" x2="100%"y2="50%" id="a"><stop stopColor="#5D0202" stopOpacity="0"offset="0%"/><stop stopColor="#5D0202" stopOpacity=".498"offset="100%"/></linearGradient></defs><circle fill="url(#a)"transform="matrix(0 -1 -1 0 640 640)" cx="320" cy="320" r="320"fillRule="evenodd" opacity=".309"/></svg>
           </div>
 
           <div className="citiesTextContainer">
@@ -32,9 +32,9 @@ export default function Cities() {
               src="/assets/shared/desktop/illustration-australia.svg"
               width={202}
               height={202}
-              alt="Happy man working in front of his tablet"
+              alt="Sidney opera illustration"
             />
-            <svg className="citiesBackgroundCentered" width="215" height="220" viewBox="0 0 650 550" xmlns="http://www.w3.org/2000/svg"><defs><linearGradient x1="0%" y1="50%" x2="100%"y2="50%" id="a"><stop stopColor="#5D0202" stopOpacity="0"offset="0%"/><stop stopColor="#5D0202" stopOpacity=".498"offset="100%"/></linearGradient></defs><circle fill="url(#a)"transform="matrix(0 -1 -1 0 640 640)" cx="320" cy="320" r="320"fillRule="evenodd" opacity=".309"/></svg>
+            <svg className="citiesBackgroundCentered" width="210" height="220" viewBox="0 0 650 550" xmlns="http://www.w3.org/2000/svg"><defs><linearGradient x1="0%" y1="50%" x2="100%"y2="50%" id="a"><stop stopColor="#5D0202" stopOpacity="0"offset="0%"/><stop stopColor="#5D0202" stopOpacity=".498"offset="100%"/></linearGradient></defs><circle fill="url(#a)"transform="matrix(0 -1 -1 0 640 640)" cx="320" cy="320" r="320"fillRule="evenodd" opacity=".309"/></svg>
           </div>
 
           <div className="citiesTextContainer">
@@ -51,9 +51,9 @@ export default function Cities() {
               src="/assets/shared/desktop/illustration-united-kingdom.svg"
               width={202}
               height={202}
-              alt="Man back view working on his office"
+              alt="Tower bridge UK illustration"
             />
-            <svg className="citiesBackgroundRight" width="215" height="220" viewBox="0 0 650 550" xmlns="http://www.w3.org/2000/svg"><defs><linearGradient x1="0%" y1="50%" x2="100%"y2="50%" id="a"><stop stopColor="#5D0202" stopOpacity="0"offset="0%"/><stop stopColor="#5D0202" stopOpacity=".498"offset="100%"/></linearGradient></defs><circle fill="url(#a)"transform="matrix(0 -1 -1 0 640 640)" cx="320" cy="320" r="320"fillRule="evenodd" opacity=".309"/></svg>
+            <svg className="citiesBackgroundRight" width="210" height="220" viewBox="0 0 650 550" xmlns="http://www.w3.org/2000/svg"><defs><linearGradient x1="0%" y1="50%" x2="100%"y2="50%" id="a"><stop stopColor="#5D0202" stopOpacity="0"offset="0%"/><stop stopColor="#5D0202" stopOpacity=".498"offset="100%"/></linearGradient></defs><circle fill="url(#a)"transform="matrix(0 -1 -1 0 640 640)" cx="320" cy="320" r="320"fillRule="evenodd" opacity=".309"/></svg>
           </div>            
 
           <div className="citiesTextContainer">
